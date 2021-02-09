187f88ba0fa1f62f140cf7e1b515e27513132c1c
ericvini <ericcobain3@gmail.com>
2021-02-09 17:18:19 -0300

    alteraçoes

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -10,7 +10,7 @@ function Projects(){
       <div className='container'>
       <div className='card'>
       <h3>project-recipes-app</h3>
-      <p className='card-text'>Foi desenvolvido um app de receitas, utilizando o que há de mais moderno dentro do ecossistema React: Hooks e Context API!
+      <p className='card-text'>Foi desenvolvido em  grupo um app de receitas, utilizando o que há de mais moderno dentro do ecossistema React: Hooks e Context API!
 
 Nela será possível ver, buscar, filtrar, favoritar e acompanhar o processo de preparação de receitas e drinks!
 
